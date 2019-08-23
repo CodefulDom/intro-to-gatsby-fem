1c69b0b556e01d3a3d4f22985952885fc4acc4fe
Dom Hallan <dom@codefuldom.com>
2019-08-22 20:53:10 -0500

    :art: adding styles in to home and adding layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,53 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react'
+import { Global, css } from '@emotion/core'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+const Layout = ({ children }) => (
+  <>
+    <Global
+      styles={css`
+        * {
+          box-sizng: border-box;
+          margin: 0;
         }
-      }
-    }
-  `)
 
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
-}
+        * + * {
+          margin-top: 1rem;
+        }
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+        body,
+        html {
+          margin: 0;
+          color: #555;
+          font-family: -apple-system;
+          font-size: 18px;
+          line-height: 1.4;
+
+          /* remove margin for the main div that gatsby renders into */
+          > div {
+            margin-top: 0;
+          }
+          :is(h1, h2, h3, h4, h5, h6) {
+            color: #222;
+            line-height: 1.1;
+
+            + * {
+              margin-top: 0.5rem;
+            }
+          }
+
+          strong {
+            color: #222;
+          }
+
+          li {
+            margin-top: 0.25rem;
+          }
+        }
+      `}
+    />
+    <header />
+    <main>{children}</main>
+  </>
+)
 
 export default Layout
